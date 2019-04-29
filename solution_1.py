1e8055fb856d39e122b574e0c469e730eab63157
Rahul <rahulatrkm@gmail.com>
2019-04-29 23:40:37 +0530

    Completed problem 2

--- solution_1.py
+++ solution_1.py
@@ -20,8 +20,8 @@ def sqrt(number):
         else:
             low = mid
 
-print ("Pass" if  (3 == sqrt(9)) else "Fail")
-print ("Pass" if  (0 == sqrt(0)) else "Fail")
+print ("Pass" if  (3 == sqrt(9)) else "Fail")   # Pass
+print ("Pass" if  (0 == sqrt(0)) else "Fail")   # Pass
 print ("Pass" if  (4 == sqrt(16)) else "Fail")
 print ("Pass" if  (1 == sqrt(1)) else "Fail")
 print ("Pass" if  (5 == sqrt(27)) else "Fail")
